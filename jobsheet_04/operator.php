b88da42bf0df92b372d5d1ab9b943b754ced3a57
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-18 15:16:56 +0700

    add pt 3.2_pembanding

--- jobsheet_04/operator.php
+++ jobsheet_04/operator.php
@@ -3,7 +3,7 @@
     $b = 5;
 
     //3.1 
-    echo "--- OPERATOR ARITMATIKA ---<br>";
+    echo "<b>--- OPERATOR ARITMATIKA ---</b><br>";
     $hasilTambah = $a + $b;
     $hasilKurang = $a - $b;
     $hasilKali = $a * $b;
@@ -16,5 +16,22 @@
     echo "Hasil Kali $a dan $b adalah $hasilKali<br>";
     echo "Hasil Bagi $a dan $b adalah $hasilBagi<br>";
     echo "Sisa Bagi $a dan $b adalah $sisaBagi<br>";
-    echo "Hasil Pangkat $a dan $b adalah $pangkat<br>";
+    echo "Hasil Pangkat $a dan $b adalah $pangkat<br><br>";
+
+
+    //3.2
+    echo "<br><b>--- OPERATOR PEMBANDING ---</b><br>";
+    $hasilSama = $a == $b;
+    $hasilTidakSama = $a != $b;
+    $hasilLebihKecil = $a < $b;
+    $hasilLebihBesar = $a > $b;
+    $hasilLebihKecilSama = $a <= $b;
+    $hasilLebihBesarSama = $a >= $b;
+
+    echo "Apakah $a sama dengan $b?" . ($hasilSama ? ' true' : ' false') . "<br>";
+    echo "Apakah $a tidak sama dengan $b?" . ($hasilTidakSama ? ' true' : ' false') . "<br>";
+    echo "Apakah $a lebih kecil dari $b?" . ($hasilLebihKecil ? ' true' : ' false') . "<br>";
+    echo "Apakah $a lebih besar dari $b?" . ($hasilLebihBesar ? ' true' : ' false') . "<br>";
+    echo "Apakah $a lebih kecil sama dengan $b?" . ($hasilLebihKecilSama ? ' true' : ' false') . "<br>";
+    echo "Apakah $a lebih besar sama dengan $b?" . ($hasilLebihBesarSama ? ' true' : ' false') . "<br>";
 ?>
