13324797271425d83179a0f1dc2a750608b403ae
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-18 12:46:04 +0700

    add pt 2.2_tipe data

--- jobsheet_04/tipe_data.php
+++ jobsheet_04/tipe_data.php
@@ -1,4 +1,6 @@
 <?php
+    //2.1
+    echo "--- PERTAMA ---<br>";
     $a = 10;
     $b = 5;
     $c = $a + 5;
@@ -12,4 +14,19 @@
     echo "Variabel e: {$e} <br><br>";
 
     var_dump($e);
+
+    //2.2
+    echo "<br><br>--- KEDUA ---<br>";
+    $nilaiMatematika = 5.1;
+    $nilaiIPA = 6.7;
+    $nilaiBahasaIndonesia = 9.3;
+
+    $rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+    echo "Matematika : {$nilaiMatematika} <br>";
+    echo "IPA : {$nilaiIPA} <br>";
+    echo "Bahasa Indonesia : {$nilaiBahasaIndonesia} <br>";
+    echo "Rata - rata : {$rataRata} <br><br>";
+
+    var_dump($rataRata);
 ?>
