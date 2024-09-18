25b6d3f644fa6f0ec77eb0eb15dfda0bbf6bb963
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-18 13:58:30 +0700

    add pt 3.1_aritmatika

---
+++ jobsheet_04/operator.php
@@ -0,0 +1,20 @@
+<?php 
+    $a = 10;
+    $b = 5;
+
+    //3.1 
+    echo "--- OPERATOR ARITMATIKA ---<br>";
+    $hasilTambah = $a + $b;
+    $hasilKurang = $a - $b;
+    $hasilKali = $a * $b;
+    $hasilBagi = $a / $b;
+    $sisaBagi = $a % $b;
+    $pangkat = $a ** $b;
+
+    echo "Hasil Tambah $a dan $b adalah $hasilTambah<br>";
+    echo "Hasil Kurang $a dan $b adalah $hasilKurang<br>";
+    echo "Hasil Kali $a dan $b adalah $hasilKali<br>";
+    echo "Hasil Bagi $a dan $b adalah $hasilBagi<br>";
+    echo "Sisa Bagi $a dan $b adalah $sisaBagi<br>";
+    echo "Hasil Pangkat $a dan $b adalah $pangkat<br>";
+?>
