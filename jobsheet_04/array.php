5bda881400ceae9a619feea03be07ea3888f06bc
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-09-23 21:33:30 +0700

    add 5.2_array2

--- jobsheet_04/array.php
+++ jobsheet_04/array.php
@@ -12,4 +12,25 @@
     }
 
     echo "Daftar nilai siswa yang lulus : " . implode(', ', $nilaiLulus);
+
+
+    //5.2
+    echo "<br><br><b>--- Penerapan Array Karyawan ---</b><br>";
+    $daftarKaryawan = [
+        ['Alice', 7],
+        ['Bob', 3],
+        ['Charlie', 9],
+        ['David', 5],
+        ['Eva', 6],
+    ];
+
+    $karyawanPengalamanLimaTahun = [];
+
+    foreach ($daftarKaryawan as $karyawan) {
+        if ($karyawan[1] > 5) {
+            $karyawanPengalamanLimaTahun[] = $karyawan[0];
+        }
+    }
+
+    echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun : " . implode(', ', $karyawanPengalamanLimaTahun);
 ?>
