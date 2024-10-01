20cfe9b42ecce96fdffaf6a179e41721c51dc55e
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-01 21:06:45 +0700

    modif pt 2_associative

--- jobsheet_05/praktik_php/array_2.php
+++ jobsheet_05/praktik_php/array_2.php
@@ -6,16 +6,37 @@
         <title></title>
     </head>
     <body>
-        <?php
-            $Dosen = [
-                'nama' => 'Elok Nur Hamdana',
-                'domisili' => 'Malang',
-                'jenis_kelamin' => 'Perempuan'
-            ];
+        <style>
+            table {
+                border: 1px solid white;
+            }
+            th, td {
+                padding: 15px;
+                text-align: center;
+            }
+            th {
+                background-color: pink;
+            }
+        </style>
+        <table border = "1", cellpadding = "5">
+            <thead>
+                <th>Nama</th>
+                <th>Domisili</th>
+                <th>Jenis Kelamin</th>
+            </thead>
+            <tbody>
+                <?php
+                    $Dosen = [
+                        'nama' => 'Elok Nur Hamdana',
+                        'domisili' => 'Malang',
+                        'jenis_kelamin' => 'Perempuan'
+                    ];
 
-            echo "Nama : {$Dosen ['nama']} <br>";
-            echo "Domisili : {$Dosen ['domisili']} <br>";
-            echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
-        ?>
+                    echo "<td> {$Dosen ['nama']} </td>";
+                    echo "<td> {$Dosen ['domisili']} </td>";
+                    echo "<td> {$Dosen ['jenis_kelamin']} </td>";
+                ?>
+            </tbody>
+        </table>
     </body>
 </html>
