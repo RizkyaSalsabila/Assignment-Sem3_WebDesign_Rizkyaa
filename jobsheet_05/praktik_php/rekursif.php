32cc9e195030aa29edfc08f7a1f048b0060b1052
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-01 23:22:40 +0700

    add pt 5.2_fungsi rekurisif

--- jobsheet_05/praktik_php/rekursif.php
+++ jobsheet_05/praktik_php/rekursif.php
@@ -1,10 +1,23 @@
 <?php
     //no 9
-    function tampilkanHaloDunia() {
-        echo "Halo Dunia! <br>";
+    // function tampilkanHaloDunia() {
+    //     echo "Halo Dunia! <br>";
 
-        tampilkanHaloDunia();
+    //     tampilkanHaloDunia();
+    // }
+
+    // tampilkanHaloDunia();
+
+
+    //no.10
+    function tampilkanAngka(int $jumlah, int $indeks = 1) {
+        echo "Perulangan ke - {$indeks} <br>";
+
+        //kondisi panggil diri sendiri selama $indeks <= $jumlah
+        if ($indeks < $jumlah) {
+            tampilkanAngka($jumlah, $indeks + 1);
+        }
     }
 
-    tampilkanHaloDunia();
+    tampilkanAngka(20);
 ?>
