dce6553c7618492949482fe81ffe3af3f1ebce47
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-01 21:38:33 +0700

    add pt 4.1_fungsi

---
+++ jobsheet_05/praktik_php/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+    function perkenalan() {
+        echo "Assalamualaikum, ";
+        echo "Perkenalkan nama saya Bella (*-*) <br>";
+        echo "Senang berkenalan dengan Anda<br><br>";
+    }
+
+    //panggil fungsi perkenalan()
+    perkenalan();
+    perkenalan();
+?>
