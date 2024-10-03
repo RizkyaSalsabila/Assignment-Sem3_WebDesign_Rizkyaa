f838265c622c0560ca83e0493477c28491b90536
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-03 19:52:02 +0700

    add pt 10.1_penerapan date

---
+++ jobsheet_05/dasar_web/date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Date PHP</title>
+    </head>
+    <body>
+        <h3>Date</h3>
+        <?php
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";
+            echo "Today is " . date("1");
+        ?>
+    </body>
+</html>
