10c11cbf3afdbd993afc433efad1da822f18aeb1
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-18 21:37:44 +0700

    add pt 1.2_isset

--- jobsheet_07/isset.php
+++ jobsheet_07/isset.php
@@ -1,8 +1,19 @@
 <?php
+    //1.1
+    echo "<b>-- PERCOBAAN 1.1 --</b><br>";
     $umur;
     if (isset($umur) && $umur >= 18) {
         echo "Anda sudah dewasa.";
     } else {
         echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+    }
+
+    //1.2
+    echo "<br><br><b>-- PERCOBAAN 1.2 --</b><br>";
+    $data = array("nama" => "Jane", "usia" => 25);
+    if (isset($data["nama"])) {
+        echo "Nama : " . $data["nama"];
+    } else {
+        echo "Variabel 'nama' tidak ditemukan dalam array";
     }
 ?>
