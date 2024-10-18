912a84179f46f11e7a0943f6d381acd323d8cf09
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-10-18 21:43:45 +0700

    add pt 2.1_pengecekan empty 'if'

---
+++ jobsheet_07/empty.php
@@ -0,0 +1,10 @@
+<?php
+    //2.1
+    echo "<b>-- PERCOBAAN 2.1 --</b><br>";
+    $myArray = array();     //array masih kosong
+    if (empty($myArray)) {
+        echo "Array tidak terdefinisi atau kosong.";
+    } else {
+        echo "Array terdefinisi dan tidak kosong.";
+    }
+?>
