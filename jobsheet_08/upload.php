959e382028db2eaa6bcc5e72fc12488711b08182
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-12-02 22:58:05 +0700

    add 1.4_modify

--- jobsheet_08/upload.php
+++ jobsheet_08/upload.php
@@ -1,22 +1,22 @@
 <?php
     if (isset($_POST["submit"])) {
-        $targetDirectory = "uploads/";  //direktori tujuan untuk menyimpan file
-        $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+        $targetDirectory = "documents/";  //direktori tujuan untuk menyimpan file
+        $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
 
         //tambahan kode
-        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-        $maxFileSize = 5 * 1024 * 1024;
+        $documentsFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        $allowedExtensions = array("txt", "pdf", "doc", "docx");
+        $maxFileSize = 10 * 1024 * 1024;
         
-        if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-                echo "File berhasil diunggah."; "<br><br>";
-                echo "<img src = '" . $targetFile . "' width = '200' alt = 'Thumbnail'>";
+        if (in_array($documentsFileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+            if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+                echo "Dokumen berhasil diunggah.";
+                // echo "<img src = '" . $targetFile . "' width = '200' alt = 'Thumbnail'>";
             } else {
-                echo "Gagal mengunggah file.";
+                echo "Gagal mengunggah dokumen.";
             }
         } else {
-            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+            echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
         }
     }
 ?>
