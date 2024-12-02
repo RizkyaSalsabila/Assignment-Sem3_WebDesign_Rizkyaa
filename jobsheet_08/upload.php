04d7094abb06eee31cb2dadf96dae7e214747ddf
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-12-02 18:49:29 +0700

    add 1.2_modify

--- jobsheet_08/upload.php
+++ jobsheet_08/upload.php
@@ -3,10 +3,19 @@
         $targetDirectory = "uploads/";  //direktori tujuan untuk menyimpan file
         $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
 
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah.";
+        //tambahan kode
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxFileSize = 5 * 1024 * 1024;
+        
+        if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+                echo "File berhasil diunggah.";
+            } else {
+                echo "Gagal mengunggah file.";
+            }
         } else {
-            echo "Gagal mengunggah file.";
+            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
         }
     }
 ?>
