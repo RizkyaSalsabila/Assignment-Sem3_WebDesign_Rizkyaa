77878b228e8e751d7432051a6b1a5a8109b6ac9d
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-12-13 15:24:23 +0700

    add pt 10.3_proses session home

---
+++ jobsheet_09/homeSession.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            session_start();
+
+            if ($_SESSION['status'] == 'login') {
+                echo "Selamat Datang " . $_SESSION['username'];
+        ?>
+            <br> <a href = "sessionLogout.php">Log Out</a>
+        <?php
+            } else {
+                echo "Anda belum login. Silahkan "
+        ?>
+                <a href = "sessionLoginForm.html">Log In</a>
+        <?php
+            }
+        ?>
+    </body>
+</html>
