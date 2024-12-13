88ac93c797dfb9361abff809be0f161471ae1691
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-12-13 12:10:16 +0700

    add pt 1.3_pengisian tabel

---
+++ jobsheet_09/inputTable.php
@@ -0,0 +1,31 @@
+<?php
+    //untuk menghubungkan ke database
+    include 'koneksi.php';
+
+    //input user and password
+    $username = "admin";
+    $password = "123";
+
+    //penerapan md5
+    $hashed_password = md5($password);
+
+    //kode SQL untuk input
+    $sql = "INSERT INTO user 
+    (id, username, password, level) VALUES
+    (2, '$username', '$hashed_password', 2)";
+
+    //cek koneksi
+    if (!$connect) {
+        die("Koneksi gagal : " . mysqli_connect_error());
+    }
+
+    //cek penambahan data
+    if (mysqli_query($connect, $sql)) {
+        echo "Data berhasil dimasukkan";
+    } else {
+        echo "Gagal saat memasukkan data : " . mysqli_error($connect);
+    }
+
+    //tutup koneksi
+    $connect->close();
+?>
