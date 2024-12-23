14506d92b419d264873ff5a3d200a56e139a643f
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-12-24 00:43:57 +0700

    add pt 5.3_mengubah edit data  dg bootstrap

--- jobsheet_11/crud/edit.php
+++ jobsheet_11/crud/edit.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Edit Data Anggota</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     </head>
     <body>
         <?php
@@ -15,57 +15,42 @@
             mysqli_close($connect);
         ?>
 
-        <div class="container">
+        <div class="container mt-4">
             <h2>Edit Data Anggota</h2>
-            <form action="proses1.php?aksi=ubah" method="POST">
-                <input type="hidden" name="id" value="<?php 
-                        echo $row['id']; 
-                    ?>
-                ">
-                    
-                <label for="nama">Nama : </label>
-                <input type="text" name="nama" id="nama" 
-                value="<?php 
-                        echo $row['nama']; 
-                    ?>
-                " required>
-                    
-                <label for="jenis_kelamin">Jenis Kelamin : </label>
-                <div class="radio-group">
-                    <input type="radio" name="jenis_kelamin" value="L" id="laki" 
-                        <?php
-                            if ($row['jenis_kelamin'] === 'L') 
-                                echo 'checked';
-                        ?>
-                    required>
-                    <label for="laki">Laki-laki</label>
-
-                    <input type="radio" name="jenis_kelamin" value="P" id="perempuan" 
-                    <?php
-                            if ($row['jenis_kelamin'] === 'P') 
-                                echo 'checked';
-                        ?>
-                    required>
-                    <label for="perempuan">Perempuan</label>
+            <form action="proses.php?aksi=ubah" method="POST">
+                <input type="hidden" name="id" value="<?php echo $row['id']; ?>"> 
+                <div class='form-group'>
+                    <label for="nama">Nama : </label>
+                    <input type="text" class="form-control" name="nama" id="nama" value="<?php echo $row['nama']; ?> " required> 
                 </div>
-                <br>
-                <label for="alamat">Alamat : </label>
-                <input type="text" name="alamat" id="alamat" 
-                value="<?php 
-                        echo $row['alamat']; 
-                    ?>
-                " required><br>
-                    
-                <label for="no_telp">No. Telp : </label>
-                <input type="text" name="no_telp" id="no_telp" 
-                value="<?php 
-                        echo $row['no_telp']; 
-                    ?>
-                " required>
-                    
-                <button type="submit">Simpan Perubahan</button>
-                <a href="index.php" class="btn-kembali">Kembali</a>
+                <div class='form-group mt-3'>
+                    <label for="jenis_kelamin">Jenis Kelamin : </label>
+                    <div class='form-check'>
+                        <input type="radio" class='form-check-input' name="jenis_kelamin" value="L" id="laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>   
+                        <label class='form-check-label' for="laki">Laki-laki</label>
+                    </div> 
+                    <div class='form-check'>
+                        <input type="radio" class='form-check-input' name="jenis_kelamin" value="P" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
+                        <label class='form-check-label' for="perempuan">Perempuan</label>
+                    </div> 
+                </div>
+                <div class='form-group mt-3'>
+                    <label for="alamat">Alamat : </label>
+                    <input type="text" class='form-control' name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+                </div>
+                <div class='form-group mt-3'>
+                    <label for="no_telp">No. Telp : </label>
+                    <input type="text" class='form-control' name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
+                </div> 
+                <button type="submit" class='btn btn-primary mt-5'>Simpan Perubahan</button>
             </form>
+            
+            <a href="index.php" class="btn btn-secondary mt-2">Kembali</a>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     </body>
 </html>
